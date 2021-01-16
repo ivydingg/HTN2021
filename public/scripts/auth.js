52edf3c431b3409a26f5966b39396d8d7c3ccb21
n34wu <n34wu@uwaterloo.ca>
2021-01-16 16:49:19 -0500

    working email/user authentication

---
+++ public/scripts/auth.js
@@ -0,0 +1,122 @@
+function handleSignUp() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+  // Create user with email and pass.
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+
+  /**
+ * Sends an email verification to the user.
+ */
+function sendEmailVerification() {
+    firebase.auth().currentUser.sendEmailVerification().then(function() {
+      // Email Verification sent!
+      alert('Email Verification Sent!');
+    });
+  }
+  
+
+function toggleSignIn() {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut();
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+  }
+}
+
+
+function sendPasswordReset() {
+  var email = document.getElementById('email').value;
+  firebase.auth().sendPasswordResetEmail(email).then(function() {
+    // Password Reset Email Sent!
+    alert('Password Reset Email Sent!');
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/invalid-email') {
+      alert(errorMessage);
+    } else if (errorCode == 'auth/user-not-found') {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
+
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+  // Listening for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log("logged in: " + email);
+    } else {
+      // User is signed out.
+      console.log("logged out");
+    }
+  });
+  
+  if (document.getElementById('sign-in') !== null)
+    document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
+
+  if (document.getElementById('sign-up') !== null)
+    document.getElementById('sign-up').addEventListener('click', handleSignUp, false);
+
+  if (document.getElementById('password-reset') !== null)
+    document.getElementById('password-reset').addEventListener('click', sendPasswordReset, false);
+}
+
+window.onload = function() {
+  initApp();
+};
