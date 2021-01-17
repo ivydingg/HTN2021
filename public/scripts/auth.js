25e3498eecc91f6fdf75eccec45b97c7801ede18
n34wu <n34wu@uwaterloo.ca>
2021-01-17 02:47:00 -0500

    connect submit idea page to firestore

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,4 +1,4 @@
-var domain = "https://hack-the-north-2020.web.app";
+var domain = "https://cloudcastle.space";
 //var domain = "https://localhost:5000";
 
 function handleSignUp() {
@@ -105,12 +105,15 @@ function sendPasswordReset() {
   });
 }
 
+
+
 /**
  * initApp handles setting up UI event listeners and registering Firebase auth listeners:
  *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
  *    out, and that is where we update the UI.
  */
 function initApp() {
+
   // Listening for auth state changes.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
